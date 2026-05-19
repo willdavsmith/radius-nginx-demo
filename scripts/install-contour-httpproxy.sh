1a2c5e454c34095a748a9d3f8f0f2e215801afb6
willdavsmith <willdavsmith@gmail.com>
2026-05-19 11:51:45 -0700

    Grant Radius HTTPProxy recipe permissions

--- scripts/install-contour-httpproxy.sh
+++ scripts/install-contour-httpproxy.sh
@@ -9,3 +9,36 @@ kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
 kubectl wait --timeout=5m -n "${CONTOUR_NAMESPACE}" deployment/contour --for=condition=Available
 kubectl rollout status --timeout=5m -n "${CONTOUR_NAMESPACE}" daemonset/envoy
 kubectl get service -n "${CONTOUR_NAMESPACE}" envoy
+
+cat <<'EOF' | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: radius-contour-httpproxy-recipes
+rules:
+  - apiGroups:
+      - projectcontour.io
+    resources:
+      - httpproxies
+    verbs:
+      - get
+      - list
+      - watch
+      - create
+      - update
+      - patch
+      - delete
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: radius-contour-httpproxy-recipes
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: radius-contour-httpproxy-recipes
+subjects:
+  - kind: ServiceAccount
+    name: dynamic-rp
+    namespace: radius-system
+EOF
